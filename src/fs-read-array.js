df960c6a222e0b27f95ff412166613415f81e261
nampdn <nampdn@protonmail.com>
2017-11-23 17:56:07 +0700

    feat(core): add encoding as second parameter
    BREAKING CHANGE: move callback to third param, insert `encoding` as second param

--- src/fs-read-array.js
+++ src/fs-read-array.js
@@ -10,7 +10,11 @@ const isArray = require('is-array');
  * @param {Array<string>} fileList Array of absolute path to read
  * @param {Function} callback
  */
-module.exports = (fileList, callback) => {
+module.exports = (fileList, encoding = 'utf8', callback) => {
+  if (typeof encoding !== 'string') {
+    callback = encoding;
+    encoding = 'utf8';
+  }
   if (isArray(fileList)) {
     return new Promise((resolve, reject) => {
       const promises = [];
@@ -18,7 +22,7 @@ module.exports = (fileList, callback) => {
 
       while (i--) {
         const promise = new Promise((resolve, reject) => {
-          fs.readFile(fileList[i], 'utf8', (error, data) => {
+          fs.readFile(fileList[i], encoding, (error, data) => {
             if (error) {
               reject(error);
             } else {
