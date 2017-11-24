2cd864f2e56bb98a84921d6fae7ac18a3f73966b
nampdn <nampdn@protonmail.com>
2017-11-24 15:43:04 +0700

    chore(types): add typing for list-files

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,11 +4,12 @@ export interface FSArray {
   /**
    * Read file content in an absolute array path.
    * @param {Array<string>} fileList List of absolute files path
-   * @param {string|option} encoding Encoding when read file
-   * @param {(error: any, contents: Array<any>) => {}} callback Callback function
+   * @param {string} [encoding] Encoding when read file
+   * @param {(error: any, contents: Array<any>) => {}} [callback] Callback function
    * @return {Promise<Array<any>>}
    */
-  readArray(fileList: Array<string>, encoding?: string, callback?: (error: any, contents: Array<any>) => {}): Promise<Array<any>>;
+  readFiles(fileList: Array<string>, encoding?: string, callback?: (error: any, contents: Array<any>) => {}): Promise<Array<any>>;
+  listFiles(dir: string, ext?: string): Promise<Array<string>>;
 }
 
 declare const fsArray: FSArray;
